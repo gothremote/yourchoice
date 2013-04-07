cdd15531c31f0ff7ac0e7cd23e4d880ce1bde50d
gothremote <matgrillere@gmail.com>
2013-04-07 21:19:45 +0200

    mise en forme header

--- includes/entete.php
+++ includes/entete.php
@@ -6,3 +6,18 @@
 <LINK rel="stylesheet" type="text/css" href="style/yourchoice.css">
 </head>
 <body>
+<div class="header">
+<img class="gauche" src="images/pointint.png">
+<img class="droite" src="images/pointint.png">
+<h1> Bienvenue sur l'aide decisionnelle </h1>
+</div>
+<div class="menu">
+<ul>
+<li><a href="Prepose" >Questions préposées </a> </li>
+<li><a href="Inutile" >Questions inutiles </a></li>
+<li><a href="Improbable" >Questions improbables </a></li>
+<li><a href="Sexe" >Questions sexuelles</a></li>
+<li><a href="Question" > Poses-ta question </a></li>
+<li><a href="Favorite" > Question favorite </a></li>
+</ul>
+</div>
--- pages/index/index.php
+++ pages/index/index.php
@@ -1,24 +1,12 @@
 <?php 
 entete("Accueil");
 ?>
-<div class="background">
 <center>Bonjour, bienvenue sur le meilleur site de l'Oise !
 <p>
 Nous sommes Matthieu et Marine, enchantés de te rencontrer.
 </center>
 MENU
 <p>
-<div class="menu">
-<ul>
-<li><a href="Prepose" >Questions préposées </a> </li>
-<li><a href="Inutile" >Questions inutiles </a></li>
-<li><a href="Improbable" >Questions improbables </a></li>
-<li><a href="Sexe" >Questions sexuelles</a></li>
-<li><a href="Question" > Poses-ta question </a></li>
-<li><a href="Favorite" > Question favorite </a></li>
-</ul>
-</div>
-</div>
 <?php 
 basdepage();
 ?>
