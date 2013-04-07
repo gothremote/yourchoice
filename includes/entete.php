da2ae0e7633940bd64730e3fa70ceba287aa9788
gothremote <matgrillere@gmail.com>
2013-04-07 20:04:38 +0200

    Debut du menu

--- includes/entete.php
+++ includes/entete.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
 <meta charset="UTF-8">
-<title> <?php echo $titre; ?></title>
+<title> <?php echo $titre; ?> </title>
 <LINK rel="stylesheet" type="text/css" href="style/yourchoice.css">
 </head>
 <body>
--- pages/index/index.php
+++ pages/index/index.php
@@ -8,14 +8,16 @@ Nous sommes Matthieu et Marine, enchantés de te rencontrer.
 </center>
 MENU
 <p>
-
-<div class="bouton"> <a href="Prepose" >Questions préposées </a> </div>
-<div class="bouton"> <a href="Inutile" >Questions inutiles </a></div>
-<div class="bouton"> <a href="Improbable" >Questions improbables </a></div>
-<div class="bouton"> <a href="Sexe" >Questions sexuelles</a></div>
-<div class="bouton"> <a href="Question" > Poses-ta question </a></div>
-<div class="bouton"> <a href="Favorite" > Question favorite </a></div>
-
+<div class="menu">
+<ul>
+<li><a href="Prepose" >Questions préposées </a> </li>
+<li><a href="Inutile" >Questions inutiles </a></li>
+<li><a href="Improbable" >Questions improbables </a></li>
+<li><a href="Sexe" >Questions sexuelles</a></li>
+<li><a href="Question" > Poses-ta question </a></li>
+<li><a href="Favorite" > Question favorite </a></li>
+</ul>
+</div>
 </div>
 <?php 
 basdepage();
